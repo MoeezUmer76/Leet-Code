065b0464c133fce7c5a5bb134a91eb7e8a8969eb
MoeezUmer76 <moeezu76@gmail.com>
2025-06-08 20:18:17 +0500

    Added container with most water to medium folder

---
+++ LeetCodeSolutions/LeetCodeSolutions/medium/ContainerWithMostWater.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        if (height.size() < 2) return 0;
+
+        int left = 0;
+        int right = height.size() - 1;
+        int maxarea = 0;
+
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+
+            maxarea = max(maxarea, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxarea;
+    }
+};
